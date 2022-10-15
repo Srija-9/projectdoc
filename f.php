242c579224873f4db6cb2446315df0d852326a01
Srija-9 <114080992+Srija-9@users.noreply.github.com>
2022-10-15 14:46:48 +0530

    Update f.php

--- f.php
+++ f.php
@@ -1,12 +1,3 @@
-<?php
-$pdo=new PDO('mysql:host=localhost;port=3306;dbname=mydb','SRIJA','1234');
-if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['user_id'])){
-	$sql="INSERT INTO usersev(name,email,password,user_id) VALUES (:name,:email,:password,:user_id)";
-	echo("<pre>\n".$sql."\n</pre>\n");
-	$stmt=$pdo->prepare($sql);
-	$stmt->execute(array(':name'=> $_POST['name'],':email'=>$_POST['email'],':password'=>$_POST['password'],':user_id'=>$_POST['user_id']));
-}
-?>
 <html><head></head><body>
 <p>Add A User</p>
 <form method="post">
@@ -16,5 +7,14 @@ if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) 
 <p>User_id:<input type="text" name=user_id></p>
 <p><input type="submit" value="Add New"/></p>
 </form>
+<?php
+$pdo=new PDO('mysql:host=localhost;port=3306;dbname=mydb','SRIJA','1234');
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['user_id'])){
+	$sql="INSERT INTO usersev(name,email,password,user_id) VALUES (:name,:email,:password,:user_id)";
+	echo("<pre>\n".$sql."\n</pre>\n");
+	$stmt=$pdo->prepare($sql);
+	$stmt->execute(array(':name'=> $_POST['name'],':email'=>$_POST['email'],':password'=>$_POST['password'],':user_id'=>$_POST['user_id']));
+}
+?>	
 </body>
 </html>
